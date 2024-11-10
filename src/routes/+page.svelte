9ebd6c5f1cf07b5ad6d40026be2cb9231fefe7c5
Martin <wienc.martin@gmail.com>
2024-11-10 17:39:46 -0500

    misc improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,48 @@
   import type { PageData } from './$types';
   import * as Avatar from '$lib/components/ui/avatar/index.js';
   import * as Card from '$lib/components/ui/card/index.js';
-  import { LoaderCircleIcon, PlayIcon } from 'lucide-svelte';
+  import {
+    CornerDownLeftIcon,
+    CornerDownRightIcon,
+    LoaderCircleIcon,
+    PauseIcon,
+    PlayIcon,
+  } from 'lucide-svelte';
   import Button from '$lib/components/ui/button/button.svelte';
   import { invoke } from '@tauri-apps/api/core';
   import type { NotifyState } from '$lib/types/notify-state';
   import { listen } from '@tauri-apps/api/event';
   import { differenceInMinutes } from 'date-fns';
+  import { onDestroy, onMount } from 'svelte';
 
   const { data }: { data: PageData } = $props();
   const user = $derived(data.user);
+  let interval = $state<number | null>(null);
+  onMount(async () => {
+    interval = setInterval(updateLastNotifiedDisplay, 1000);
+    const state: NotifyState = await invoke('is_notifying');
+    notifying = state.isRunning;
+  });
+  onDestroy(() => {
+    if (interval) clearInterval(interval);
+  });
   let notifying = $state(false);
   let lastNotified = $state<Date | null>(null);
-  let lastNotifiedDisplay = $derived.by(() => {
+  let updateLastNotifiedDisplay = () => {
     if (!lastNotified) return 'n/a';
-    if (differenceInMinutes(new Date(), lastNotified) < 1) return 'Just now';
-    if (differenceInMinutes(new Date(), lastNotified) < 60)
-      return `${differenceInMinutes(new Date(), lastNotified)} minutes ago`;
+    let minAgo = differenceInMinutes(new Date(), lastNotified);
+    if (minAgo < 1) {
+      lastNotifiedDisplay = 'Just now';
+      return;
+    }
+    if (minAgo < 60) {
+      lastNotifiedDisplay = `${differenceInMinutes(new Date(), lastNotified)} minute${minAgo > 1 ? 's' : ''} ago`;
+      return;
+    }
 
-    return `Over ${Math.floor(differenceInMinutes(new Date(), lastNotified) / 60)} hours ago`;
-  });
+    lastNotifiedDisplay = `Over ${Math.floor(minAgo / 60)} hours ago`;
+  };
+  let lastNotifiedDisplay = $state(updateLastNotifiedDisplay());
   async function start() {
     const state: NotifyState = await invoke('start_notifying');
     notifying = state.isRunning;
@@ -35,26 +58,36 @@
 </script>
 
 <main class="flex flex-col items-center justify-center h-[100vh]">
-  <Card.Root class="border-none !bg-transparent">
+  <Card.Root class="border-none !bg-transparent w-full">
     <Card.Header>
       <Card.Title class="flex flex-row gap-2 items-center">
         <Avatar.Root>
           <Avatar.Image src={user.discordAvatar} alt="{user.name} avatar" />
           <Avatar.Fallback><LoaderCircleIcon /></Avatar.Fallback>
         </Avatar.Root>
-        <span class="text-3xl font-bold">{user.name}</span>
+        <div class="flex flex-col -mt-1">
+          <span class="text-sm text-gray-400 leading-tight">
+            notifying
+            <CornerDownLeftIcon
+              class="rotate-[270deg] inline-block size-3 translate-y-[1px]"
+            />
+          </span>
+          <span class="text-3xl font-bold leading-6">{user.name}</span>
+        </div>
       </Card.Title>
     </Card.Header>
     <Card.Content>
-      <div class="flex flex-row justify-between w-full">
+      <div class="flex flex-row gap-4 w-full [&>*]:text-sm">
         <div>Last Notified</div>
         <span class="text-gray-400">{lastNotifiedDisplay}</span>
       </div>
     </Card.Content>
     <Card.Footer class="flex flex-row justify-between">
       {#if notifying}
-        <Button class="w-full" onclick={stop}>
-          <LoaderCircleIcon class="size-4 animate-spin" /> Pause Notifying
+        <Button class="w-full group [&>*]:transition-all" onclick={stop}>
+          <LoaderCircleIcon class="size-4 animate-spin group-hover:hidden" />
+          <PauseIcon class="size-4 hidden group-hover:block" />
+          Pause Notifying
         </Button>
       {:else}
         <Button class="w-full" onclick={start}>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,11 +2,14 @@
   import DiscordIcon from '$lib/img/discord.svg';
   import Button from '$lib/components/ui/button/button.svelte';
 
-  function onClick() {}
+  function onClick() {
+    // TODO: open browser to discord login
+  }
 </script>
 
 <main class="flex flex-row gap-2 w-full items-center justify-center h-[100vh]">
-  <Button onclick={onClick}
-    ><img src={DiscordIcon} alt="discord logo" class="size-6" /> Continue with Discord</Button
-  >
+  <Button onclick={onClick}>
+    <img src={DiscordIcon} alt="discord logo" class="size-6" />
+    Continue with Discord
+  </Button>
 </main>
